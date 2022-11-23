9bd25cfd4a363467abef33be36623284c56f46c7
Gabor Racz <gbrcz235+gitea@gmail.com>
2022-11-23 16:15:42 +0000

    Initial commit

---
+++ CommandLineParameters.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace RegDB
+{
+    internal class CommandLineParameters
+    {
+        public string Operation { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{Operation},{Key},{Value},";
+        }
+
+        public static CommandLineParameters Parse(string[] args)
+        {
+            if (args == null) { throw new ArgumentNullException(nameof(args)); }
+            CommandLineParameters parameters = new();
+            for (var i = 0; i < args.Length; i++)
+            {
+                //Console.WriteLine($"args[{i}]={args[i]}");
+                var arg = args[i].ToString().ToLower(CultureInfo.InvariantCulture);
+
+                switch (arg)
+                {
+                    case "-op":
+                        parameters.Operation = args[i+1].ToString().ToLower(CultureInfo.InvariantCulture);
+                        break;
+
+                    case "-key":
+                        parameters.Key = args[i+1];
+                        break;
+
+                    case "-value":
+                        parameters.Value = args[i+1];
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            return parameters;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System.Runtime.Versioning;
+
+namespace RegDB
+{
+    internal class Program
+    {
+        [SupportedOSPlatform("Windows")]
+        static void Main(string[] args)
+        {
+            try
+            {
+                var cmdArgs = CommandLineParameters.Parse(args);
+                switch (cmdArgs.Operation)
+                {
+                    case "set":
+                        Set(cmdArgs.Key, cmdArgs.Value);
+                        Environment.Exit(0);
+                        break;
+                    case "get":
+                        var val = Get(cmdArgs.Key);
+                        Console.WriteLine(val);
+                        Environment.Exit(0);
+                        break;
+                    case "unset":
+                        UnSet(cmdArgs.Key);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                Environment.Exit(1);
+            }
+        }
+
+        [SupportedOSPlatform("Windows")]
+        private static string Get(string key)
+        {
+            using RegistryKey regKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\regdb");
+            var value = regKey.GetValue(key).ToString();
+            if (!string.IsNullOrEmpty(value))
+            {
+                return value;
+            }
+            else
+            {
+                Console.WriteLine($"Key '{key}' not found.");
+                Environment.Exit(1);
+                return null;
+            }
+        }
+
+        [SupportedOSPlatform("Windows")]
+        private static void Set(string key, string value)
+        {
+            using RegistryKey regKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\regdb");
+            regKey.SetValue(key, value);
+        }
+
+        [SupportedOSPlatform("Windows")]
+        private static void UnSet(string key)
+        {
+            using RegistryKey regKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\regdb");
+            regKey.SetValue(key, string.Empty);
+        }
+    }
+}
